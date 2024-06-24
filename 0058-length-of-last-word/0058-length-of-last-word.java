1d470bdbb927ef2038d8ae942aae7292a71c13ab
Vedant Namdeo <147383363+vedantnamdeo@users.noreply.github.com>
2024-06-24 11:40:14 +0530

    Time: 0 ms (100%), Space: 41.5 MB (52.75%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.java
+++ 0058-length-of-last-word/0058-length-of-last-word.java
@@ -1,9 +1,6 @@
 class Solution {
     public int lengthOfLastWord(String s) {
-        // if(s.indexOf(s.length()).matches(" "))
-        // {
-        //     s=
-        // }
+      
        s=s.trim();
        return ((s.substring(s.lastIndexOf(" ")+1,s.length())).length());
         
